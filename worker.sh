6c5bd4fd4e8e2952ab9be5e88f56c0e44b3dc062
Craig Quiter <cquiter@gmail.com>
2019-08-05 14:56:56 -0700

    Add standard bash flags

--- worker.sh
+++ worker.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -e  # Abort script at first error, when a command exits with non-zero status (except in until or while loops, if-tests, list constructs)
+set -u  # Attempt to use undefined variable outputs error message, and forces an exit
+set -x  # Similar to verbose mode (-v), but expands commands
+set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
+
 pip3 install -r requirements.txt
 
 cp problem_worker_supervisor.conf /etc/supervisor/conf.d/
