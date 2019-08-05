efbf104d29ed95335915fa41f1ad292ed9ffe377
Craig Quiter <cquiter@gmail.com>
2019-08-05 15:52:05 -0700

    Maybe we use pleaserun

--- worker.sh
+++ worker.sh
@@ -5,16 +5,7 @@ set -u  # Attempt to use undefined variable outputs error message, and forces an
 set -x  # Similar to verbose mode (-v), but expands commands
 set -o pipefail  # Causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value.
 
-/usr/bin/python3.7 -m pip install -r requirements.txt
-
-cp problem_worker_supervisor.conf /etc/supervisor/conf.d/
-
-# Just to be sure
-sudo service supervisor start
-
-supervisorctl stop deepdrive-problem-worker
-supervisorctl reread
-supervisorctl update
-supervisorctl start deepdrive-problem-worker
-
-/usr/bin/python3.7 -u /usr/local/src/problem-worker/worker.py
+DIR=`dirname "$0"`
+echo ${DIR}
+/usr/bin/python3.7 -m pip install -r ${DIR}/requirements.txt
+/usr/bin/python3.7 -u ${DIR}/problem-worker/worker.py
