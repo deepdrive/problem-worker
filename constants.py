90c8c7d6f67b78c944ecda9ec8675d35679ad828
Craig Quiter <cquiter@gmail.com>
2019-10-04 16:38:46 -0700

    Run botleague ci on circle

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import os
 
-SIM_IMAGE_BASE_TAG = 'deepdriveio/private:deepdrive-sim-package'
+SIM_PACKAGE_IMAGE_TAG = 'deepdriveio/private:deepdrive-sim-package'
+DEEPDRIVE_BUILD_IMAGE_TAG = 'deepdriveio/deepdrive:ci'
 STACKDRIVER_LOG_NAME = 'deepdrive-worker'
